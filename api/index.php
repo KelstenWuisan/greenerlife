0d3a6e5e4eee094f015de672db32e9d9e1cf7b52
KelstenWuisan <kelsten.wuisan3@gmail.com>
2024-12-18 02:29:22 +0700

    FIx 5

--- api/index.php
+++ api/index.php
@@ -1,3 +1,27 @@
 <?php
 
-require __DIR__ . '/../public/app.php';
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Maintenance mode
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Autoload dependencies
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap the application
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
